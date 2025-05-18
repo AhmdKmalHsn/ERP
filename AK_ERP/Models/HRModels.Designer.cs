430d8f90ae3acbfa69e167274cdb9002478963c3
AhmdKmalHsn <86407920+AhmdKmalHsn@users.noreply.github.com>
2025-05-18 12:46:35 +0300

    2025-05-18

--- AK_ERP/Models/HRModels.Designer.cs
+++ AK_ERP/Models/HRModels.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\hp\Desktop\.net Sources\AK_HR\newHR\Models\HRModels.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\hp\Documents\GitHub\ERP\AK_ERP\Models\HRModels.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
